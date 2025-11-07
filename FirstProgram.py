1447dcdc5978688b20cdbf9f6e9503d969de579b
unknown <itsmebhatnagar.29@gmail.com>
2025-11-07 23:13:37 +0530

    Updated FirstProgram.py

--- FirstProgram.py
+++ FirstProgram.py
@@ -48,7 +48,7 @@ def api_command():
     elif "open amazon" in query:
         return jsonify({"response": "Opening Amazon...", "action": "open_amazon"})
 
-    elif "open chatgpt" in query:
+    elif "open chat GPT" in query:
         return jsonify({"response": "Opening Chatgpt...", "action": "open_chatgpt"})
 
     elif "open spotify" in query:
